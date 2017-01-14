215258c93745034a0eedeeda9507304ac2ddedd6
KadirCeyhan <kadir.ceyhan@outlook.com.tr>
2017-01-14 17:08:03 +0200

    Create index.php

---
+++ index.php
@@ -0,0 +1,44 @@
+<!DOCTYPE HTML>
+<html lang="tr-TR">
+<head>
+	<meta charset="UTF-8">
+	<meta http-equiv="X-UA-Compatible" content="IE=edge">
+	<meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
+	
+	<title>Beklemeden Geç v1.1</title>
+	<link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/twitter-bootstrap/3.3.7/css/bootstrap.min.css" />
+	<link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/4.7.0/css/font-awesome.min.css" />
+	<link rel="stylesheet" href="assets/css/custom.min.css?v=1.1" />
+	<link rel="shortcut icon" href="http://work.kadirceyhan.com/favicon.ico" type="image/x-icon" />
+</head>
+<body>
+	<div class="container">
+		<div class="title text-center">
+			<h1>Adf.ly Ve Link.TL İçin Geçiş Sistemi</h1>
+		</div>
+		<div class="form-inline text-center">
+			<div class="form-group">
+				<input class="url" type="text" />
+			</div>
+			<button type="submit" class="btn btn-action">Çevir</button>
+		</div>
+		<div class="checkbox text-center">
+			<label><input id="autoOpen" type="checkbox"> Çevirdikden Sonra Siteyi Yeni Sekmede Aç</label>
+		</div>
+		<div class="response text-center">
+		</div>
+		<div class="about">
+			<div class="about-title text-center">
+				<h2>Bilgilendirme</h2>
+				<hr style="width: 300px;"/>
+				<p>© 2017 <a href="https://wmaraci.com/forum/uyeler/69230-kadirceyhan"> KadirCeyhan </a> Tarafından Yapılmıştır. Script Sürümü v1.1</p>
+				<p>Bu araç ile Adf.ly ve Link.TL sistemleri ile kısaltılmış linklere doğrudan erişebilirsiniz.</p>
+				<p>Reklamlar, virüsler ve bekleme aşamalarını yok eder. Yönlendirilecek asıl linke hızlıca ulaşırsınız. Tamamen ücretsizdir.</p>
+			</div>
+		</div>
+	</div>
+	<script src="https://cdnjs.cloudflare.com/ajax/libs/jquery/3.1.1/jquery.min.js"></script>
+	<script src="https://cdnjs.cloudflare.com/ajax/libs/twitter-bootstrap/3.3.7/js/bootstrap.min.js"></script>
+	<script src="assets/js/custom.min.js?v=1.1"></script>
+</body>
+</html>	
