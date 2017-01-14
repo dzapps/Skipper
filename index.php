c245e504f24c38e213c177f7bdf11be469454773
KadirCeyhan <kadir.ceyhan@outlook.com.tr>
2017-01-14 17:16:22 +0200

    Index Dosyası

--- index.php
+++ index.php
@@ -5,10 +5,10 @@
 	<meta http-equiv="X-UA-Compatible" content="IE=edge">
 	<meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
 	
-	<title>Beklemeden Geç v1.1</title>
+	<title>Beklemeden Geç v1.1,0</title>
 	<link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/twitter-bootstrap/3.3.7/css/bootstrap.min.css" />
 	<link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/4.7.0/css/font-awesome.min.css" />
-	<link rel="stylesheet" href="assets/css/custom.min.css?v=1.1" />
+	<link rel="stylesheet" href="assets/css/custom.min.css?v=1.0" />
 	<link rel="shortcut icon" href="http://work.kadirceyhan.com/favicon.ico" type="image/x-icon" />
 </head>
 <body>
@@ -31,7 +31,7 @@
 			<div class="about-title text-center">
 				<h2>Bilgilendirme</h2>
 				<hr style="width: 300px;"/>
-				<p>© 2017 <a href="https://wmaraci.com/forum/uyeler/69230-kadirceyhan"> KadirCeyhan </a> Tarafından Yapılmıştır. Script Sürümü v1.1</p>
+				<p>© 2017 <a href="https://wmaraci.com/forum/uyeler/69230-kadirceyhan"> KadirCeyhan </a> Tarafından Yapılmıştır. Script Sürümü v1.0</p>
 				<p>Bu araç ile Adf.ly ve Link.TL sistemleri ile kısaltılmış linklere doğrudan erişebilirsiniz.</p>
 				<p>Reklamlar, virüsler ve bekleme aşamalarını yok eder. Yönlendirilecek asıl linke hızlıca ulaşırsınız. Tamamen ücretsizdir.</p>
 			</div>
@@ -39,6 +39,6 @@
 	</div>
 	<script src="https://cdnjs.cloudflare.com/ajax/libs/jquery/3.1.1/jquery.min.js"></script>
 	<script src="https://cdnjs.cloudflare.com/ajax/libs/twitter-bootstrap/3.3.7/js/bootstrap.min.js"></script>
-	<script src="assets/js/custom.min.js?v=1.1"></script>
+	<script src="assets/js/custom.min.js?v=1.0"></script>
 </body>
 </html>
