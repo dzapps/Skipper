4734cec33179e7f2ce5ace575e1169a3a2eff4c7
KadirCeyhan <kadir.ceyhan@outlook.com.tr>
2017-01-15 16:18:18 +0200

    v1.2 Güncellendi

--- index.php
+++ index.php
@@ -5,16 +5,16 @@
 	<meta http-equiv="X-UA-Compatible" content="IE=edge">
 	<meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
 	
-	<title>Beklemeden Geç v1.1,0</title>
+	<title>Skipper v1.2</title>
 	<link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/twitter-bootstrap/3.3.7/css/bootstrap.min.css" />
 	<link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/4.7.0/css/font-awesome.min.css" />
-	<link rel="stylesheet" href="assets/css/custom.min.css?v=1.0" />
+	<link rel="stylesheet" href="assets/css/custom.min.css?v=1.2" />
 	<link rel="shortcut icon" href="http://work.kadirceyhan.com/favicon.ico" type="image/x-icon" />
 </head>
 <body>
 	<div class="container">
 		<div class="title text-center">
-			<h1>Adf.ly Ve Link.TL İçin Geçiş Sistemi</h1>
+			<h1>Adf.ly, Link.TL, Adfoc.us İçin Geçiş Sistemi</h1>
 		</div>
 		<div class="form-inline text-center">
 			<div class="form-group">
@@ -31,14 +31,14 @@
 			<div class="about-title text-center">
 				<h2>Bilgilendirme</h2>
 				<hr style="width: 300px;"/>
-				<p>© 2017 <a href="https://wmaraci.com/forum/uyeler/69230-kadirceyhan"> KadirCeyhan </a> Tarafından Yapılmıştır. Script Sürümü v1.0</p>
-				<p>Bu araç ile Adf.ly ve Link.TL sistemleri ile kısaltılmış linklere doğrudan erişebilirsiniz.</p>
+				<p>© 2017 <a href="https://wmaraci.com/forum/uyeler/69230-kadirceyhan"> KadirCeyhan </a> Tarafından Yapılmıştır. Script Sürümü v1.2</p>
+				<p>Bu araç ile Adf.ly, Link.TL, Adfoc.us sistemleri ile kısaltılmış linklere doğrudan erişebilirsiniz.</p>
 				<p>Reklamlar, virüsler ve bekleme aşamalarını yok eder. Yönlendirilecek asıl linke hızlıca ulaşırsınız. Tamamen ücretsizdir.</p>
 			</div>
 		</div>
 	</div>
 	<script src="https://cdnjs.cloudflare.com/ajax/libs/jquery/3.1.1/jquery.min.js"></script>
 	<script src="https://cdnjs.cloudflare.com/ajax/libs/twitter-bootstrap/3.3.7/js/bootstrap.min.js"></script>
-	<script src="assets/js/custom.min.js?v=1.0"></script>
+	<script src="assets/js/custom.min.js?v=1.2"></script>
 </body>
-</html>
+</html>	
